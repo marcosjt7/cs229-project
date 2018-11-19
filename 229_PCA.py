6e7d8b0e4d325f67930238b2d69bc3d73fffd68f
Marcos Torres <marcosjt7@gmail.com>
2018-11-18 19:47:22 -0800

    renamed 229_PCA.py

--- 229_PCA.py
+++ 229_PCA.py
@@ -26,7 +26,7 @@ def main():
     test_X = pca.transform(test_X)
     #logistic regression
     print "LR fitting"
-    lr = LogisticRegression(solver='lbfgs')
+    lr = LogisticRegression(solver='lbfgs', max_iter=1000)
     lr.fit(train_X, train_Y)
     print "LR predicting"
     preds = lr.predict(test_X)
@@ -35,21 +35,6 @@ def main():
 
 
     
-    '''
-    pca_class = PCA(egm_matrix)
-    pca_class.calculate_mu()
-    print ("calc mu")
-    pca_class.subtract_mean_from_examples()
-    print ("subtracted mean")
-    pca_class.calc_variance()
-    print("calculated variance")
-    pca_class.rescale_values_with_variance()
-    print ("rescale values")
-    pca_class.form_eigen_subparts()
-    print("formed eigen")
-    reduced_vectors = pca_class.form_new_vectors()
-    np.savetxt("pca_vectors.txt", reduced_vectors)
-    '''
 
 if __name__ == "__main__":
     main()
