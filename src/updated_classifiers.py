a8f45854ebfa85c440f8e7235f54c34da4a47ed9
Marcos Torres <marcosjt7@gmail.com>
2018-12-13 22:49:22 -0800

    modified updated_classifiers.py

--- src/updated_classifiers.py
+++ src/updated_classifiers.py
@@ -28,6 +28,7 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import f1_score
 from sklearn.preprocessing import StandardScaler
 from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix
 
 #custom written utilities
 # from format import load_BioAge1HO
@@ -51,7 +52,11 @@ def PCA_classifier(PCA_param):
     return Pipeline(steps)
 
 def LR_classifier():
-    return  LogisticRegression(solver='lbfgs', max_iter=MAX_ITERS)
+    steps = [
+        ('scl', StandardScaler()),
+        ('clf', LogisticRegression(solver='lbfgs', max_iter=MAX_ITERS))
+    ]
+    return Pipeline(steps)
 
 #parameters: alpha (0, 0.0001, 0.1), penalty ('elasticnet','l1','l2')
 #tol? used 1e-13 w/ default alpha = 0.0001 and 1e-3 here
@@ -63,10 +68,10 @@ def sgd_classifier(alpha_val, penalty_type):
     ]
     return Pipeline(steps)
 
-def svm_classifier():
+def svm_classifier(gamma_value):
     steps = [
         ('scl', StandardScaler()),
-        ('svc', SVC(kernel='linear', shrinking=False))
+        ('svc', SVC(C=0.2, kernel='rbf', gamma=gamma_value, shrinking=False))
     ]
     return Pipeline(steps)
 
@@ -77,11 +82,11 @@ def tree_classifier(depth):
     ]
     return Pipeline(steps)
 
-def adaBoost_classifier():
+def adaBoost_classifier(learn_rate):
     steps = [
         ('scl', StandardScaler()),
-        ('ada', AdaboostClassifier(DecisionTreeClassifier(max_depth=2),
-            n_estimators=100, learning_rate=1.0, random_state=0))
+        ('ada', AdaBoostClassifier(DecisionTreeClassifier(max_depth=4),
+            n_estimators=100, learning_rate=learn_rate, random_state=0))
     ]
     return Pipeline(steps)
 
@@ -112,7 +117,6 @@ def test_model(model, X, y): #modify for rf
     avg_rec = 0.0
     avg_roc_auc = 0.0
     avg_f1 = 0.0
-    avg_num_features = 0
     #cross validation
     cv = KFold(n_splits=K_CV_SPLITS)
     for train, test in cv.split(X, y):
@@ -120,21 +124,13 @@ def test_model(model, X, y): #modify for rf
         model.fit(X[train], y[train])
         y_train_pred = model.predict(X[train])
         y_pred = model.predict(X[test])
-        #y_pos_probs = model.predict_proba(X[test])[:,1]
         #evaluate performance
         avg_train_acc += accuracy_score(y[train], y_train_pred)
         avg_test_acc += accuracy_score(y[test], y_pred)
         avg_prec += precision_score(y[test], y_pred)
         avg_rec += recall_score(y[test], y_pred)
-        #avg_roc_auc += roc_auc_score(y[test], y_pos_probs)
         avg_f1 += f1_score(y[test], y_pred)
 
-        # rf_feature_weights = model.feature_importances_
-        # curr_num_features_used = 0
-        # for weight in  rf_feature_weights:
-        #     if weight >= 0:
-        #         curr_num_features_used += 1
-        # avg_num_features += curr_num_features_used
     #average performance scores
     avg_train_acc /= K_CV_SPLITS
     avg_test_acc /= K_CV_SPLITS
@@ -142,10 +138,9 @@ def test_model(model, X, y): #modify for rf
     avg_rec /= K_CV_SPLITS
     avg_roc_auc /= K_CV_SPLITS
     avg_f1 /= K_CV_SPLITS
-    avg_num_features /= K_CV_SPLITS
 
     #return [avg_acc, avg_prec, avg_rec, avg_f1, avg_roc_auc]
-    return [avg_train_acc, avg_test_acc, avg_prec, avg_rec, avg_f1, avg_num_features]
+    return [avg_train_acc, avg_test_acc, avg_prec, avg_rec, avg_f1]
 
 def evaluate_hyperparams(hyperparams_arr, classifier, X, y, 
                         clf_name, param_name):
@@ -169,11 +164,33 @@ def evaluate_hyperparams(hyperparams_arr, classifier, X, y,
     print ("recs: " + str(recs))
     print ("f1s: " + str(f1s))
 
-    plt.plot(hyperparams_arr, f1s, 'b2-')
-    plt.ylabel('F1 score')
+    plt.plot(hyperparams_arr, recs, 'b2-')
+    plt.ylabel('Recall score')
     plt.xlabel(param_name)
-    plt.title("F1 scores for " + clf_name + " Classifier")
+    plt.title("Recall scores for " + clf_name + " Classifier")
     plt.show()
+
+def run_on_test_data(test_model, train_X, train_Y, test_X, test_Y, test_name):
+    print (test_name)
+    test_model.fit(train_X, train_Y)
+    test_pred = test_model.predict(test_X)
+    train_pred = test_model.predict(train_X)
+
+    train_acc = accuracy_score(train_Y, train_pred)
+    test_acc = accuracy_score(test_Y, test_pred)
+    prec_score = precision_score(test_Y, test_pred)
+    rec_score = recall_score(test_Y, test_pred)
+    test_f1_score = f1_score(test_Y, test_pred)
+    print ("1's in train_pred: " + str(train_pred.tolist().count(1)))
+    print ("1's in test_pred: " + str(test_pred.tolist().count(1)))
+    print ("test_acc: " + str(test_acc))
+    print ("train_acc: " + str(train_acc))
+    print ("prec_score: " + str(prec_score))
+    print ("rec_score: " + str(rec_score))
+    print ("test_f1_score: " + str(test_f1_score))
+    conf_matrix = confusion_matrix(test_Y, test_pred)
+    print ("confustion matrix is: " + str(conf_matrix))
+
 
 def main():
     #load_BioAge1HO()
@@ -182,41 +199,131 @@ def main():
     print("parsing data...")
     patients, X, y = get_data()
     print("done.")
-    #training models with different parameters
+
+    #form test and train set
+    num_ex, num_features = X.shape
+    train_X_arr = []
+    train_Y_arr = []
+    test_X_arr = []
+    test_Y_arr = []
+    num_neg_test_ex = 64
+    num_pos_test_ex = 42
+    for ind in range(num_ex):
+        lbl = y[ind]
+        if num_pos_test_ex > 0 and lbl == 1:
+            test_Y_arr.append(1)
+            test_X_arr.append(X[ind])
+            num_pos_test_ex -= 1
+        elif num_neg_test_ex > 0 and lbl == 0:
+            test_Y_arr.append(0)
+            test_X_arr.append(X[ind])
+            num_neg_test_ex -= 1
+        else:
+            train_Y_arr.append(lbl)
+            train_X_arr.append(X[ind])
+
+    train_X = np.array(train_X_arr)
+    train_Y = np.array(train_Y_arr)
+    test_X = np.array(test_X_arr)
+    test_Y = np.array(test_Y_arr)
+
 
     #random forests
-    n_estimators_arr = [35, 40, 50, 75, 90, 100, 115]
-    evaluate_hyperparams(n_estimators_arr, random_forest_classifier,
-                            X, y, "Random Forests", "Number of trees per forest")
-
+    # n_estimators_arr = [50, 75, 90, 95, 100, 105, 110,115]
+    # evaluate_hyperparams(n_estimators_arr, random_forest_classifier,
+                            # train_X, train_Y, "Random Forests", "Number of trees per forest")
+    # rf_clf = random_forest_classifier(100)
+    # run_on_test_data(rf_clf, train_X, train_Y, test_X, test_Y, 'random forest test')
     #sgd w/ l2 regularization
-    alphas = alphas = [0.0001, 0.001, 0.01, 0.1, 0.7, 1, 5]
-    # evaluate_hyperparams(alphas, sgd_classifier, X, y, 
+    alphas = [0.0001, 0.001, 0.01, 0.1, 0.7, 1, 5]
+    # evaluate_hyperparams(alphas, sgd_classifier, train_X, train_Y 
                         # "SGD w/ L2 regularization", "alpha values")
+
+
 
     #bagging
     max_features_arr = [17000, 18500, 20000, 20500, 21000, 21100, 21200, 22000]
-    # evaluate_hyperparams(max_features_arr, bagging_classifier, X, y,
+    # evaluate_hyperparams(max_features_arr, bagging_classifier, train_X, train_Y,
                         # "Bagging", "Maximum Features for Estimator")
+    # bagging_clf = bagging_classifier(21000)
+    # run_on_test_data(bagging_clf, train_X, train_Y, test_X, test_Y, 'bagging test')
+
+    #adaboost
+    learn_rates = [0.2, 0.4, 0.5, 0.7, 0.9, 1.0]
+    # evaluate_hyperparams(learn_rates, adaBoost_classifier, train_X, train_Y, 
+    					# "AdaBoost", "Learning Rate")
     
+    #sigmoid
+    # sigmoid_penalty_terms = [0.2, 0.4, 0.5, 0.7, 0.75, 0.85, 1.0]
+    # evaluate_hyperparams(sigmoid_penalty_terms, svm_classifier, train_X, train_Y,
+    					# "Sigmoid Kernel", "Penalty term")
+    sigmoid_coef0_arr = [0.0, 0.2, 0.25, 0.3, 0.4, 0.5, 0.7]
+    # evaluate_hyperparams(sigmoid_coef0_arr, svm_classifier, train_X, train_Y,
+    					# "Sigmoid Kernel", "Constant value")
+    # sigmoid_clf = svm_classifier(0.0)
+    # run_on_test_data(sigmoid_clf, train_X, train_Y, test_X, test_Y, 'sigmoid kernel test')
+
+    #polynomial
+    degrees = [1,2,3,4]
+    # evaluate_hyperparams(degrees, svm_classifier, train_X, train_Y,
+                        # "Polynomial Kernel", "Degree of Polynomial")
+    # poly_svm = svm_classifier(1)
+    # run_on_test_data(poly_svm, train_X, train_Y, test_X, test_Y, 'poly test')
+
+    #rbf svm
+    rbf_gamma_arr = [0.1, 0.2, 0.4, 0.55, 0.6, 0.75, 0.8, 0.85, 0.9]
+    # rbf_penalty_terms = [0.2, 0.4, 0.5, 0.7, 0.75, 0.85]
+    # evaluate_hyperparams(rbf_gamma_arr, svm_classifier, train_X, train_Y,
+    					# "RBF kernel", 'Gamma values')
+    # rbf_clf = svm_classifier()
+    # run_on_test_data(rbf_clf, train_X, )
 
     #PCA
-    retained_variance = [0.5, 0.6, 0.75, 0.85, 0.95]
-    # evaluate_hyperparams(retained_variance, PCA_classifier, X, y,
-                        # "PCA", "Variance Retained")
+    # retained_variance = [0.5, 0.6, 0.75, 0.85, 0.95]
+    num_components = [75, 80, 85, 90, 95, 100, 110, 115, 120, 125, 130, 135, 140]
+    # evaluate_hyperparams(num_components, PCA_classifier, train_X, train_Y,
+                        # "PCA", "Number of Components Retained")
+    # pca_clf = PCA_classifier(110)
+    # run_on_test_data(pca_clf, train_X, train_Y, test_X, test_Y, 'pca test')
 
     #decision trees
     max_depths = [1,2,3,4,5,6]
     # max_features = ['auto', 'sqrt', None]
-    # evaluate_hyperparams(max_depths, tree_classifier, X, y,
+    # evaluate_hyperparams(max_depths, tree_classifier, train_X, train_Y,
                         # "Decision Tree", "Maximum Depth")
+    # dt_clf = tree_classifier(4)
+    # run_on_test_data(dt_clf, train_X, train_Y, test_X, test_Y, "decision tree test")
+
 
     # lr_model = LR_classifier()
-    # lr_results = test_model(lr_model, X, y)
+    # lr_results = test_model(lr_model, train_X, train_Y)
     # print ("lr results:")
     # print ("avg_train_acc: " + str(lr_results[0]))
     # print ("avg_test_acc: " + str(lr_results[1]))
+    # print ("avg_prec: " + str(lr_results[2]))
+    # print ("avg_recall: " + str(lr_results[3]))
     # print ("f1_score: " + str(lr_results[4]))
+
+    #lr train/test
+    # test_model = LogisticRegression(solver='lbfgs', max_iter=MAX_ITERS)
+    test_model = LR_classifier()
+    test_model.fit(train_X, train_Y)
+    test_pred = test_model.predict(test_X)
+    train_pred = test_model.predict(train_X)
+    train_acc = accuracy_score(train_Y, train_pred)
+    test_acc = accuracy_score(test_Y, test_pred)
+    prec_score = precision_score(test_Y, test_pred)
+    rec_score = recall_score(test_Y, test_pred)
+    lr_f1_score = f1_score(test_Y, test_pred)
+    print ("1's in train_pred: " + str(train_pred.tolist().count(1)))
+    print ("1's in test_pred: " + str(test_pred.tolist().count(1)))
+    print ("test_acc: " + str(test_acc))
+    print ("train_acc: " + str(train_acc))
+    print ("prec_score: " + str(prec_score))
+    print ("rec_score: " + str(rec_score))
+    print ("f1_score: " + str(lr_f1_score))
+    lr_conf_matrix = confusion_matrix(test_Y, test_pred)
+    print ("lr confusion matrix is: " + str(lr_conf_matrix))
 
     #SGD
     # l1_eval_scores = []
@@ -225,13 +332,13 @@ def main():
     # alphas = [0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.12, 0.13, 0.14]
     # for alpha in alphas:
     #     l1_model = sgd_classifier(alpha, 'l1')
-    #     l1_eval_scores.append(test_model(l1_model, X, y))
+    #     l1_eval_scores.append(test_model(l1_model, train_X, train_Y))
 
     #     l2_model = sgd_classifier(alpha, 'l2')
-    #     l2_eval_scores.append(test_model(l2_model, X, y))
+    #     l2_eval_scores.append(test_model(l2_model, train_X, train_Y))
 
     #     elasticnet_model = sgd_classifier(alpha, 'elasticnet')
-    #     elasticnet_eval_scores.append(test_model(elasticnet_model, X, y))
+    #     elasticnet_eval_scores.append(test_model(elasticnet_model, train_X, train_Y))
     # l1_results = np.array(l1_eval_scores)
     # l1_train_accs = l1_results[:,0]
     # l1_test_accs = l1_results[:,1]
@@ -270,11 +377,21 @@ def main():
     # print ("elasticnet_recs: " + str(elasticnet_recs))
     # print ("elasticnet_f1s: " + str(elasticnet_f1s))
 
-    # plt.plot(alphas, l1_f1s, 'g^-', alphas, l2_f1s, 'ro-', alphas, elasticnet_f1s, 'b2-')
-    # plt.ylabel('f1 score')
+    # plt.plot(alphas, l1_recs, 'g^-', alphas, l2_recs, 'ro-', alphas, elasticnet_recs, 'b2-')
+    # plt.ylabel('recall score')
     # plt.xlabel("regularization alpha values")
-    # plt.title("F1 scores for SGD Classifier with Regularization")
+    # plt.title("Recall scores for SGD Classifier with Regularization")
     # plt.show()
+
+    #l2 scores
+    # l2_sgd = sgd_classifier(0.06, 'l2')
+    # run_on_test_data(l2_sgd, train_X, train_Y, test_X, test_Y, 'l2 sgd')
+
+    # l1_sgd = sgd_classifier(0.05, 'l1')
+    # run_on_test_data(l1_sgd, train_X, train_Y, test_X, test_Y, 'l1 sgd')
+
+    # elasticnet_sgd = sgd_classifier(0.07, 'elasticnet')
+    # run_on_test_data(elasticnet_sgd, train_X, train_Y, test_X, test_Y, 'elasticnet')
 
    
 if __name__ == "__main__":
